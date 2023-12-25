70d1949585c46a0d3b4a6332b5268d0d6c3dbd71
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2023-12-25 19:28:47 +0200

    third

---
+++ index.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.loop');
+function phones (phone) {
+        img.src = phone;
+}
+let contaner = document.querySelector('.contaner');
+function colors (color) {
+    contaner.style.background = color ;
+}
